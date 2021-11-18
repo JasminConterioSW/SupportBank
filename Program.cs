4494d738c09b5918c1e41aea9283e0f8bc397ffb
Jasmin Conterio <Jasmin.Conterio@softwire.com>
2021-11-18 11:05:02 +0000

    Open file, read and print each line

--- Program.cs
+++ Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.IO;
 
 namespace SupportBank
 {
     class Program
     {
+        
+        
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            // Open file, read in line by line
+            using (StreamReader sr = new StreamReader(@"C:\Work\Training\SupportBank\SupportBank\Transactions2014.csv"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+                
+            
+           
+            
+            
+            
         }
     }
 }
+
