0c53ca26eaf93b1bf366bdf5e1e9577778b3328d
Jasmin Conterio <Jasmin.Conterio@softwire.com>
2021-11-18 11:50:29 +0000

    Parsing data from file, not a a list of transactions yet

--- Program.cs
+++ Program.cs
@@ -1,32 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace SupportBank
 {
+    
+  class Transaction
+    {
+        public string date;
+        public string nameFrom;
+        public string nameTo;
+        public string narrative;
+        public decimal amount;
+        
+    }
+    
     class Program
     {
-        
-        
+        private List<Transaction> parsedTransactions;
         
         static void Main(string[] args)
         {
-
+            
+            
             // Open file, read in line by line
             using (StreamReader sr = new StreamReader(@"C:\Work\Training\SupportBank\SupportBank\Transactions2014.csv"))
             {
-                string line;
-                while ((line = sr.ReadLine()) != null)
+                string data;
+                string header = sr.ReadLine();
+
+                while ((data = sr.ReadLine()) != null)
                 {
-                    Console.WriteLine(line);
+                    string[] dataFields = data.Split(',');
+
+                    Transaction currentTransaction = new Transaction();
+                    currentTransaction.date = dataFields[0];
+                    currentTransaction.nameFrom = dataFields[1];
+                    currentTransaction.nameTo = dataFields[2];
+                    currentTransaction.narrative = dataFields[3];
+                    currentTransaction.amount = Convert.ToDecimal(dataFields[4]);
+
+
+                    Console.WriteLine(currentTransaction.amount);
                 }
+                
+                
+
             }
                 
-            
-           
             
             
             
         }
     }
+
+    
+    
+    
 }
 
